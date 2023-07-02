1c7f976da670d015652f67725964870371cec607
rym49 <hatanakaryoma.130@gmail.com>
2023-07-03 00:33:39 +0900

    連想配列の2次元配列から特定キーの配列を生成する

---
+++ README.md
@@ -0,0 +1,2 @@
+# PHPの勉強用
+https://zenn.dev/ad5/articles/60ca8edadc236f
--- src/main.php
+++ src/main.php
@@ -11,3 +11,23 @@
  * @license  MIT https://github.com/RyoMa99/study_php/LICENSE.txt
  * @link     https://hoge.com
  */
+
+$in = [
+  [
+    'id' => 1,
+    'code' => 'S1001',
+    'name' => '山田',
+  ],
+  [
+    'id' => 2,
+    'code' => 'S1003',
+    'name' => '鈴木',
+  ],
+  [
+    'id' => 3,
+    'code' => 'S1002',
+    'name' => '佐藤',
+  ],
+];
+
+var_dump(array_column($in, 'name'));
