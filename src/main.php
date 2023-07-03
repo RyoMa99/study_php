43dacf3f50c7b68905bd2b51beb795cba6d14978
rym49 <hatanakaryoma.130@gmail.com>
2023-07-04 01:51:04 +0900

    連想配列の2次元配列のあるカラムをキー、他カラムを値に下連想配列に加工

--- src/main.php
+++ src/main.php
@@ -13,10 +13,21 @@
  */
 
 $in = [
-  'id' => 1,
-  'code' => 'S1001',
-  'name' => '山田',
-  'pref' => 27,
+  [
+    'id' => 1,
+    'code' => 'S1001',
+    'name' => '山田',
+  ],
+  [
+    'id' => 2,
+    'code' => 'S1003',
+    'name' => '鈴木',
+  ],
+  [
+    'id' => 3,
+    'code' => 'S1002',
+    'name' => '佐藤',
+  ],
 ];
 
-var_dump(array_keys($in));
+var_dump(array_column($in, 'name', 'code'));
