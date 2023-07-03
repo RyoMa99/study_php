9619d78922eda3bcb39ca7953bb03e81acad7c18
rym49 <hatanakaryoma.130@gmail.com>
2023-07-04 01:30:41 +0900

    連想配列のキー一覧取得

--- src/main.php
+++ src/main.php
@@ -13,21 +13,10 @@
  */
 
 $in = [
-  [
-    'id' => 1,
-    'code' => 'S1001',
-    'name' => '山田',
-  ],
-  [
-    'id' => 2,
-    'code' => 'S1003',
-    'name' => '鈴木',
-  ],
-  [
-    'id' => 3,
-    'code' => 'S1002',
-    'name' => '佐藤',
-  ],
+  'id' => 1,
+  'code' => 'S1001',
+  'name' => '山田',
+  'pref' => 27,
 ];
 
-var_dump(array_column($in, 'name'));
+var_dump(array_keys($in));
