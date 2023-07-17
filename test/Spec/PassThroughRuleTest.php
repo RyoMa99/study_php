bdfb8a3e7480610d8ac131940832f2eefd9866d9
rym49 <hatanakaryoma.130@gmail.com>
2023-07-17 17:36:23 +0900

    テストケースの追加

---
+++ test/Spec/PassThroughRuleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace FizzBuzz\Spec;
+
+use PHPUnit\Framework\TestCase;
+
+class PassThroughRuleTest extends TestCase
+{
+  public function testReplace()
+  {
+    $rule = new PassThroughRule();
+    $this->assertEquals("1", $rule->replace(1));
+  }
+}
