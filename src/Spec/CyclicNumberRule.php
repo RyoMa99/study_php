8cbc25c3b30bcdda4fe4631ca92e289cbc7f0935
rym49 <hatanakaryoma.130@gmail.com>
2023-07-17 17:31:08 +0900

    テスト失敗

---
+++ src/Spec/CyclicNumberRule.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace FizzBuzz\Spec;
+
+use FizzBuzz\Core\ReplaceRuleInterface;
+
+class CyclicNumberRule implements ReplaceRuleInterface
+{
+  public function __construct(
+    private int $base,
+    private string $replacement,
+  ) {
+  }
+
+  public function replace(int $n): string
+  {
+    return "";
+  }
+}
