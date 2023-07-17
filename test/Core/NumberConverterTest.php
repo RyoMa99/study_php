41dc7e84340244c89cf1c6de476c94c254f7815c
rym49 <hatanakaryoma.130@gmail.com>
2023-07-17 16:57:03 +0900

    リファクタリング

--- test/Core/NumberConverterTest.php
+++ test/Core/NumberConverterTest.php
@@ -14,13 +14,12 @@ class NumberConverterTest extends TestCase
 
   public function testConvertWithSingleRule()
   {
-    $rule = $this->getMockBuilder(ReplaceRuleInterface::class)->getMock();
-    $rule->expects($this->atLeastOnce())
-      ->method('replace')
-      ->with(1)
-      ->willReturn("Replaced");
-
-    $fizzBuzz = new NumberConverter([$rule]);
+    $fizzBuzz = new NumberConverter([
+      $this->createMockRule(
+        expectedNumber: 1,
+        replacement: "Replaced",
+      )
+    ]);
     $this->assertEquals("Replaced", $fizzBuzz->convert(1));
   }
 
