8777cf000125cacc6c856db5d58d580d6a8de6dd
rym49 <hatanakaryoma.130@gmail.com>
2023-07-17 16:55:06 +0900

    テストケースの追加

--- test/Core/NumberConverterTest.php
+++ test/Core/NumberConverterTest.php
@@ -23,4 +23,33 @@ class NumberConverterTest extends TestCase
     $fizzBuzz = new NumberConverter([$rule]);
     $this->assertEquals("Replaced", $fizzBuzz->convert(1));
   }
+
+  public function testConvertWithFizzBuzzRule()
+  {
+
+    $fizzBuzz = new NumberConverter([
+      $this->createMockRule(
+        expectedNumber: 1,
+        replacement: "Fizz",
+      ),
+      $this->createMockRule(
+        expectedNumber: 2,
+        replacement: "Buzz",
+      ),
+    ]);
+    $this->assertEquals("FizzBuzz", $fizzBuzz->convert(1));
+  }
+
+  private function createMockRule(
+    int $expectedNumber,
+    string $replacement
+  ): ReplaceRuleInterface {
+    $rule = $this->getMockBuilder(ReplaceRuleInterface::class)->getMock();
+    $rule->expects($this->atLeastOnce())
+      ->method('replace')
+      ->with($expectedNumber)
+      ->willReturn($replacement);
+
+    return $rule;
+  }
 }
