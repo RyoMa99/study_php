1339dba4336f866c48decb8b5cc001c2d766a1d2
rym49 <hatanakaryoma.130@gmail.com>
2023-07-17 15:47:51 +0900

    テストシナリオの追加

--- test/Core/NumberConverterTest.php
+++ test/Core/NumberConverterTest.php
@@ -10,5 +10,13 @@ class NumberConverterTest extends TestCase
   {
     $fizzBuzz = new NumberConverter([]);
     $this->assertEquals("", $fizzBuzz->convert(1));
+    $this->assertEquals("", $fizzBuzz->convert(2));
+    $this->assertEquals("Fizz", $fizzBuzz->convert(3));
+    $this->assertEquals("", $fizzBuzz->convert(4));
+    $this->assertEquals("Buzz", $fizzBuzz->convert(5));
+    $this->assertEquals("Fizz", $fizzBuzz->convert(6));
+    $this->assertEquals("Buzz", $fizzBuzz->convert(10));
+    $this->assertEquals("FizzBuzz", $fizzBuzz->convert(15));
+    $this->assertEquals("FizzBuzz", $fizzBuzz->convert(30));
   }
 }
