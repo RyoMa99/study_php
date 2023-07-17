8b497581d5e360a8e451045f1b3ec6d2070dacc5
rym49 <hatanakaryoma.130@gmail.com>
2023-07-17 15:44:54 +0900

    Rule変更を柔軟に出来るようConverterの外に出す

--- src/Core/NumberConverter.php
+++ src/Core/NumberConverter.php
@@ -4,15 +4,13 @@ namespace FizzBuzz\Core;
 
 class NumberConverter
 {
-  public function convert(int $n): string
-  {
-    if ($n % 3 == 0 && $n % 5 == 0) {
-      return "FizzBuzz";
-    } elseif ($n % 5 == 0) {
-      return "Buzz";
-    } elseif ($n % 3 == 0) {
-      return "Fizz";
-    }
-    return (string)$n;
+  /**
+   * Undocumented function
+   *
+   * @param ReplaceRuleInterface[] $rules
+   */
+  public function __construct(
+    protected array $rules
+  ) {
   }
 }
